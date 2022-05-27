a474111375dd8191cb183da47fe3cfee589e1a8f
Zomroo <93994060+Zomroo@users.noreply.github.com>
2022-05-27 09:32:41 +0530

    Add files via upload

---
+++ FallenRobot/modules/alive.py
@@ -0,0 +1,28 @@
+import os
+import re
+import random
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from FallenRobot.events import register
+from FallenRobot import telethn as tbot
+
+
+PHOTO = [
+    "https://telegra.ph/file/197b908108db1d9f4a8f7.mp4",
+]
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ 𝗠𝗶𝘀𝗵𝗮 𝗡𝗲𝗰𝗿𝗼𝗻​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ʀᴏʀᴏɴᴏᴀ ᴢᴏʀᴏ](https://t.me/AceLadi)** \n\n"
+  TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+  TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+  TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/mishaxrobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/NexusXSupport")]]
+  ran = random.choice(PHOTO)
+  await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
+
+## Alive mod
